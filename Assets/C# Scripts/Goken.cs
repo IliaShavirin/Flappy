ee2526af3a1bb93cdfcca573a5f5762cffa68c96
DESKTOP-U29D4K4\EXPERT PC <shavirinilia@gmail.com>
2023-08-01 03:30:43 +0300

    Initialize Game

---
+++ Assets/C# Scripts/Goken.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goken : MonoBehaviour
+{
+    [SerializeField] private AudioSource Gok;
+    public float force;
+    Rigidbody2D GokuRigid;
+    public GameObject RestartButton;
+    public GameObject BackButton;
+    void Start()
+    {
+        Time.timeScale = 1;
+        GokuRigid = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GokuRigid.velocity = Vector2.up * force;
+            Gok.Play();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            Time.timeScale = 0;
+            RestartButton.SetActive(true);
+            BackButton.SetActive(true);
+        }
+    }
+}
---
+++ Assets/C# Scripts/LvlsManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LvlsManager : MonoBehaviour
+{
+    public void PlayLvl1()
+    {
+        Application.LoadLevel("Game");
+    }
+
+    public void BackToMenu()
+    {
+        Application.LoadLevel("Menu");
+    }
+}
